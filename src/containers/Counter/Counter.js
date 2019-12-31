d3eb36dc21a456e95d8dcf5dc7e7eeb8b347ed89
Michael Neely <michael@michaelneely.net>
2019-12-31 00:34:33 -0500

    Added switch statements instead of 'ifs' and learned more about redux

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -5,35 +5,14 @@ import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
-
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
-    }
-
     render () {
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 10" clicked={this.props.onAddFiveCounter}  />
+                <CounterControl label="Subtract 15" clicked={this.props.onSubtractFiveCounter}  />
             </div>
         );
     }
@@ -47,8 +26,12 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({type: 'INCREMENT'})
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
+        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
+        onAddFiveCounter: () => dispatch({type: 'ADD', value: 10}),
+        onSubtractFiveCounter: () => dispatch({type: 'SUBTRACT', value: 15})
     };
+    
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
